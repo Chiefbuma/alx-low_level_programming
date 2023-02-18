ae34b03aede7698b4b9a8846fb7a1e061beeb9e3
Chiefbuma <jorgebuma.jb@gmail.com>
2023-02-18 12:16:23 -0800

    prints using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main -a program that prints with printf
- * then returns :0
+ * Return:0
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
