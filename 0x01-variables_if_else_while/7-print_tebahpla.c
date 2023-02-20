e8553cd80268accacd89f96a1bad504ad4a3a7d1
Chiefbuma <jorgebuma.jb@gmail.com>
2023-02-20 10:35:45 -0800

    Alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - alphabet in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	while (i = 'z')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
