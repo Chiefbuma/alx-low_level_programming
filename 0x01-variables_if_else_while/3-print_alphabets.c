fe378830cd53096200a31b56e91e50764e235c03
Chiefbuma <jorgebuma.jb@gmail.com>
2023-02-20 09:29:27 -0800

    Upper and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@ int main(void)
 	int ch = 'a';
 	int CH = 'A';
 
-	while (ch < 'z')
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-	while (CH < 'Z')
+	while (CH <= 'Z')
 	{
 		putchar(CH);
 		CH++;
