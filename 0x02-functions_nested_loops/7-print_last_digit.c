d9472d5a24107782c6f5edeb712f270a62975a42
Chiefbuma <jorgebuma.jb@gmail.com>
2023-02-22 14:07:02 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "maini.h"
+/**
+ * print_last_digit - a function that prints the last digit of a number
+ * @dx: number's last digit result
+ * Return: value of the last digit
+ */
+int print_last_digit(int dx)
+{
+	int dx;
+
+	dx = (dx % 10);
+
+	if (dx < 0)
+	{
+		dx = (-1 * dx);
+	}
+
+	_putchar(dx + '0');
+	return (dx);
+}
