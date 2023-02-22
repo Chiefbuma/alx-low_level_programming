a6d21c565e78571cafef7ff18ecdeed56eb0ee7d
Chiefbuma <jorgebuma.jb@gmail.com>
2023-02-22 13:43:38 -0800

    alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
  * @c: letter
  * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
-int _isalpha(int c)
+int _isalpha(int r )
 {
-	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	if (((r >= 'a') && (r <= 'z')) || ((r >= 'A') && (r <= 'Z')))
 		return (1);
 	else
 		return (0);
