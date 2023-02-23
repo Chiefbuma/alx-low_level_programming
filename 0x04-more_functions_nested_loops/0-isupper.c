08121d80ff71209141156573308880619f865aeb
Chiefbuma <jorgebuma.jb@gmail.com>
2023-02-23 10:28:58 -0800

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+}
+	int c = i
+
+	if (i >= 'A' &&  i <= 'Z')
+		return (1);
+i	else
+		return (0);
+}
