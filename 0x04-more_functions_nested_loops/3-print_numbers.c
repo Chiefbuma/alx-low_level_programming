1b427d74512a30fbe1003726aa54c2a2b992f1c1
Chiefbuma <jorgebuma.jb@gmail.com>
2023-02-23 12:08:07 -0800

    0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 /**
  * print_numbers - print numbers from (0 to 9)
  * _putchar - print the numbers
- * Return: 0
+ * Return: void
  */
 
 void print_numbers(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
 		_putchar(a);
 	}
