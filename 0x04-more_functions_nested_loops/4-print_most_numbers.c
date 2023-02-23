f578952d738f840c8f70862d49c68f3e990d6caa
Chiefbuma <jorgebuma.jb@gmail.com>
2023-02-23 12:25:38 -0800

    skip 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _putchar - print the results
+ *
+ * print_most_numbers -  describe function
+ *
+ * Return: void
+ */
+int print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; ++i)
+	{
+		if ((i == '2' || i == '4'))
+			_putchar(i);
+	{
+		_putchar('n\');
+}
