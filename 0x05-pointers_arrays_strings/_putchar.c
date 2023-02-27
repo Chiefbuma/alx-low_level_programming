7eef176a9d7145b5b4037301a2e2dacbed665f73
Chiefbuma <jorgebuma.jb@gmail.com>
2023-02-27 09:22:58 -0800

    putchar

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include" main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints char c
+ * @c: Print this char
+ *
+ * Return On succes 1
+ * On error, -1 is returned
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1);
+}
