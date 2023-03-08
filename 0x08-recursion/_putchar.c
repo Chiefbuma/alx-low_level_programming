7985852093dde65b11b5dd762c2239230e42f8a4
Chiefbuma <jorgebuma.jb@gmail.com>
2023-03-08 04:01:27 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include" main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints char c
+ * @c: Print this char
+ *
+ * Return:1 on succes
+ * On error, -1  is returned
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
