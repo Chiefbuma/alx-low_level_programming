23cb48addf006191dadbc8e2bd6483fbd51de3be
Chiefbuma <jorgebuma.jb@gmail.com>
2023-10-07 03:19:19 -0700

    Use the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -Write a C program that prints using puts
+ * Return: 0
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
