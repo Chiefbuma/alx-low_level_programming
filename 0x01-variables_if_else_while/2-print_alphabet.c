64671e5af41660efce086b337af4df226effc4b1
Chiefbuma <jorgebuma.jb@gmail.com>
2023-10-11 05:18:16 -0700

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - putchar alphabet in lowercase.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char d = 'a'
+
+	while (d <= 'z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
