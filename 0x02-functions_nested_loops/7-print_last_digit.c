05340471b58c05464588bbabaa495e4c72da71c2
Chiefbuma <jorgebuma.jb@gmail.com>
2023-10-20 07:37:49 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,12 +5,19 @@
  * @n: the int to extract the last digit from
  * Return: value of the last digit
  */
-
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int a;
 
-	printf("%d", last_digit);
-	return (last_digit);
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
-
