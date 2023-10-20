46709a08fdf7736110579a08b3f93475c20c7a1f
Chiefbuma <jorgebuma.jb@gmail.com>
2023-10-20 01:39:20 -0700

    x10 lower aplha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet folled by new line
+ */
+void print_alphabet(void)
+{
+char x = 'a';
+int i;
+
+for (i = 0; i <= 10; i++)
+{
+
+while (x <= 'z')
+{
+	_putchar(x);
+	x++;
+}
+x = 'a'
+}
+_putchar('\n');
+}
