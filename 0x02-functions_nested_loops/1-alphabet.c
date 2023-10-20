9478ed04212d267ce93c83bb729cb0e863e5585a
Chiefbuma <jorgebuma.jb@gmail.com>
2023-10-20 01:26:32 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet folled by new line
+ */
+void print_alphabet(void)
+{
+char x = 'a';
+
+while x < = 'z'
+{
+	_putchar(x);
+	x++;
+}
+_putchar('\n')
+}
+
