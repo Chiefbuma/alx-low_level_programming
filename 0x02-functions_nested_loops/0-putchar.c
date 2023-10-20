e96ca2a80480de1a80daf7dcc4d794d23dd413e7
Chiefbuma <jorgebuma.jb@gmail.com>
2023-10-20 00:33:53 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,10 +2,19 @@
 
 /**
  * main -  prints _putchar, followed by a new line
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-_putchar("_putchar\n");
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
