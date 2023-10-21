9648e08d1d4102e4d53c1dd0921a0bdb492866d6
Chiefbuma <jorgebuma.jb@gmail.com>
2023-10-21 03:21:43 -0700

    is upper

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
+ * _isdigit - digits from 0 to 9
+ * @c: integer
+ * Return: 1 else  0
  */
 int _isdigit(int c)
 {
