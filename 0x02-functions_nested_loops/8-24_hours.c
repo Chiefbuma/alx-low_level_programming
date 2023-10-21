82633d8ae44e2d3b0f07defdbbcfdd3b91cfcaa9
Chiefbuma <jorgebuma.jb@gmail.com>
2023-10-21 01:07:48 -0700

    jack baur

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,9 +16,11 @@ while (h < 24)
 m = 0;
 while (m < 60)
 {
-_putchar(h / 10 + '0');
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
 _putchar(':');
-_putchar(m % 10 + '0');
+_putchar((m % 10) + '0');
+_putchar((m % 10) + '0');
 _putchar('\n');
 m++;
 }
