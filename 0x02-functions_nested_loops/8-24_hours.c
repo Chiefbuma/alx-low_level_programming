5e67df21bcbc5a00a83dc7a4f63b1681954a2ef4
Chiefbuma <jorgebuma.jb@gmail.com>
2023-10-21 00:40:53 -0700

    Jack baur

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include"<main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting frm morning to midnight
+ */
+
+void jack_bauer(void)
+{
+for (int h = 0; h < 24; h++)
+{
+for (int m = 0; m < 60; m++)
+{
+_putchar(num / 10 + '0');
+_putchar(':');
+_putchar(num % 10 + '0');
+_putchar('\n');
+}
+}
+}
