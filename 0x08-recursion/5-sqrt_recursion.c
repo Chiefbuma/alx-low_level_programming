805d48873f0264fb861509745c94865f70279cac
Chiefbuma <jorgebuma.jb@gmail.com>
2023-10-25 12:18:57 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,37 @@
-#include "main.h"
+#include "main.hi"
+
 /**
- * _sqrt_recursion - Calculate the natural square root of a number.
- * @n: The input number
- * Return: The square root or -1 if it does not exist.
+ * real_sqrt_recursion - square root of a number.
+ * @n: The number for which the square root is calculated.
+ * @i: Iterator for the recursive calculation.
+ * Return: The resulting square root or -1
+ */
+int real_sqrt_recursion(int n, int i);
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number for which the square root is calculated.
+ * Return: The resulting square root or -1
  */
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-{
 return (-1);
+return (real_sqrt_recursion(n, 0));
 }
-else if (n == 0 || n == 1)
+
+/**
+ * real_sqrt_recursion - square root of a number.
+ * @n: The number for which the square root is calculated.
+ * @i: Iterator for the recursive calculation.
+ * Return: The resulting square root or -1
+ */
+int real_sqrt_recursion(int n, int i)
 {
-return (n);
-}
-else
-{
-int i = 1;
-for  (i = 1; i * i <= n; i++)
-{
+if (i * i > n)
+return (-1);
 if (i * i == n)
-{
 return (i);
+return (real_sqrt_recursion(n, i + 1));
 }
-}
-return (-1);
-}
-}
+
