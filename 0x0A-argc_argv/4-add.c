5a55485390840f9dff2f5dc4229fa27368c9ecbb
Chiefbuma <jorgebuma.jb@gmail.com>
2023-10-27 11:52:49 -0700

    Write a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,27 +1,84 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-
 /**
- * main - Prints  numbers
- * @argc: The number of arguments
- * @argv: An array
- * Return: I 1. Otherwise - 0
+ * _atoi - convert to integer
+ * @s: string to be converted
+ * Return: the int converted from the string
+ */
+
+
+int _atoi(char *s)
+{
+int k, l, o, len, q, digit;
+
+k = 0;
+l = 0;
+o = 0;
+len = 0;
+q = 0;
+digit = 0;
+
+while (s[len] != '\0')
+len++;
+
+while (k < len && q == 0)
+{
+if (s[k] == '-')
+++l;
+
+if (s[k] >= '0' && s[k] <= '9')
+{
+digit = s[k] - '0';
+if (l % 2)
+digit = -digit;
+o = o * 10 + digit;
+q = 1;
+if (s[k + 1] < '0' || s[k + 1] > '9')
+break;
+q = 0;
+}
+k++;
+}
+
+if (q == 0)
+return (0);
+
+return (o);
+}
+/**
+ * main - adds  numbers
+ * @argc: arguments
+ * @argv: array
+ * Return: 0 (Success), or 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-int num, digit, sum = 0;
-for (num = 1; num < argc; num++)
+int sum, allnum, k;
+
+sum = 0;
+
+for (k = 1; k < argc; k++)
 {
-for (digit = 0; argv[num][digit]; digit++)
+for (int j = 0; argv[k][j] != '\0'; j++)
 {
-if (argv[num][digit] < '0' || argv[num][digit] > '9')
+if (argv[k][j] > '9' || argv[k][j] < '0')
 {
-printf("Error\n");
+puts("Error");
 return (1);
 }
 }
-sum += atoi(argv[num]);
+}
+
+for (k = 1; k < argc; k++)
+{
+allnum = _atoi(argv[k]);
+if (allnum >= 0)
+{
+sum += allnum;
+}
+}
+
 printf("%d\n", sum);
 return (0);
 }
+
